78461febb676a468e984a47b551ed3cd48e295bd
Luis Torres <luisdaniel.programador@gmail.com>
2017-08-25 17:54:09 -0400

    Agregado esquema de la aplicacion
    Incluido Codeigniter
    Archivo SQL
    .txt de esquema de la aplicacion

--- www/js/app.js
+++ www/js/app.js
@@ -3,31 +3,27 @@ angular.module('GATE', ['ionic', 'ngCordova'])
 .config(function ($stateProvider, $urlRouterProvider) {
 
     $stateProvider
-    .state('inicio', {
+    .state('ingreso', {
         url: '/',
         templateUrl: '../templates/inicio.html',
         controller: 'inicioController as inicio'
     })
-    .state('login', {
-        url: 'login',
-        templateUrl: '../templates/login.html',
-        controller: 'loginController as login'
+    .state('inicio', {
+        url: '/inicio',
+        templateUrl: '../templates/inicio.html',
+        controller: 'inicioController as inicio'
     })
-    .state('perfil', {
-        url: 'perfil',
-            templateUrl: '../templates/perfil.html',
-            controller: 'perfilController as perfil',
-        })
-        .state('configuracionC', {
-            url: 'configuracionC',
-            templateUrl: '../templates/configuracionCuenta.html',
-            controller: 'configuracionCController as configuracionC',
-        })
-        .state('grupos', {
-            url: 'grupos',
-            templateUrl: '../templates/grupos.html',
-            controller: 'gruposController as grupos',
-        });
+    .state('inicio/perfil', {
+        url: '/perfil',
+        templateUrl: '../templates/perfil.html',
+        controller: 'perfilController as perfil'
+    })
+    .state('inicio/configuracion', {
+        url: '/',
+        templateUrl: '../templates/inicio.html',
+        controller: 'inicioController as inicio'
+    })
+    
 
     $urlRouterProvider.otherwise('/');
 
