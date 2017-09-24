f0307684e432f1d43621041c6a0cb0d32e5086fe
Luis Torres <luisdaniel.programador@gmail.com>
2017-09-24 17:56:39 -0400

    Arreglado el composer.json

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,19 @@
 angular.module('GATE', ['ionic', 'ngCordova'])
 
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
+  })
+
   .config(function ($stateProvider, $urlRouterProvider) {
 
     $stateProvider
@@ -61,4 +75,4 @@ angular.module('GATE', ['ionic', 'ngCordova'])
 
 
     $urlRouterProvider.otherwise('/');
-});
+  });
