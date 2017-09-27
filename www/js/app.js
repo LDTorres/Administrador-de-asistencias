adbec58ec989eb85792a579123fe21d72ed596c4
fladrian <adriandev.fl@gmail.com>
2017-09-26 21:39:49 -0400

    Añadiendo el datePickerObj a mi entorno

--- www/js/app.js
+++ www/js/app.js
@@ -16,8 +16,21 @@ angular.module('GATE', ['ionic', 'ngCordova', 'ngFileUpload', 'ionic-datepicker'
 
   .config(function ($stateProvider, $urlRouterProvider, $httpProvider, ionicDatePickerProvider) {
 
-      todayLabel: 'Hoy',
-      setLabel: 'Colocar',
+    var datePickerObj = { 
+      inputDate: new Date(), 
+      setLabel: 'Colocar', 
+      todayLabel: 'Hoy', 
+      closeLabel: 'Cerrar', 
+      mondayFirst: true, 
+      weeksList: ["D", "L", "Ma", "Mi", "J", "V", "S"], 
+      monthsList: ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"], 
+      templateType: 'popup', 
+      from: new Date(2017, 1, 1), 
+      to: new Date(2030, 12, 30), 
+      showTodayButton: true, 
+      dateFormat: 'dd MMMM yyyy', 
+      closeOnSelect: false, 
+      disableWeekdays: [0, 6], };
 
     ionicDatePickerProvider.configDatePicker(datePickerObj);
 
